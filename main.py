d45400946b7ff0d508f854a93c8e52a028300eae
AndariiDev <AndariiDev@proton.me>
2026-06-14 03:47:46 +0200

    docs: add type hints

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import sys
+from typing import Dict, List
 
-from stats import get_num_words
-from stats import get_num_chars
-from stats import sorted_report
+from stats import char_dict_to_sorted_list, get_num_chars, get_num_words, sorted_report
 
-def get_book_text(file_path):
-    with open(file_path, 'r') as file:
+
+def get_book_text(file_path: str) -> str:
+    with open(file_path, "r") as file:
         return file.read()
 
-def main():
+
+def main() -> None:
     if len(sys.argv) != 2:
         print(f"Usage: python3 {sys.argv[0]} <path_to_book>")
         sys.exit(1)
@@ -17,15 +18,19 @@ def main():
     num_words = get_num_words(text)
     num_chars = get_num_chars(text)
     sorted_chars = sorted_report(num_chars)
+    sorted_list = char_dict_to_sorted_list(list(num_chars.items()))
     # print(num_words)
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {file_path}")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
+    print("----------- Sorted List ----------")
+    print(f"{sorted_list}")
     print("--------- Character Count -------")
     for item in sorted_chars:
         print(f"{item['char']}: {item['num']}")
     print("============= END ===============")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,10 +1,14 @@
-def get_num_words(text):
+from typing import Dict, List
+
+
+def get_num_words(text: str) -> int:
     num_words = text.split()
     return len(num_words)
     # print("{len(num_words)} words found in the document")
 
-def get_num_chars(text):
-    num_chars = {}
+
+def get_num_chars(text: str) -> Dict[str, int]:
+    num_chars: Dict[str, int] = {}
     text = text.lower()
     for i in text:
         if i.isalpha():
@@ -14,7 +18,22 @@ def get_num_chars(text):
                 num_chars[i] = 1
     return num_chars
 
-def sorted_report(num_chars):
-    char_counts_list = [{"char": char, "num": count} for char, count in num_chars.items()]
+
+def sorted_report(num_chars: Dict[str, int]) -> List[Dict[str, int]]:
+    char_counts_list = [
+        {"char": char, "num": count} for char, count in num_chars.items()
+    ]
     sorted_list = sorted(char_counts_list, key=lambda d: d["num"], reverse=True)
     return sorted_list
+
+
+def sort_on(sorted: tuple[str, int]) -> int:
+    return sorted[1]
+
+
+def char_dict_to_sorted_list(num_chars: tuple[str, int]) -> list[tuple[str, int]]:
+    empty_list = []
+    for i in num_chars:
+        empty_list.append(i)
+    sorted_dict = sorted(empty_list, reverse=True, key=sort_on)
+    return sorted_dict
